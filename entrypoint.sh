b9cd9ce6f00017882f2db364830de3db2419923f
iovejrj <iovejrj001@gmail.com>
2020-12-06 22:12:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,13 @@ cd /frps
 
 cat <<-EOF > /frps/frps.ini
 [common]
-bind_port = $PORT
+bind_port = 80
+dashboard_port = 443
+token = 12345678
+dashboard_user = admin
+dashboard_pwd = admin
+vhost_http_port = 10080
+vhost_https_port = 10443
 EOF
 
 echo $PORT
